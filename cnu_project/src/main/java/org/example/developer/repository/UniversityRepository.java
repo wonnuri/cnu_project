f0d4a85b72a864ed0a6d335ecf372da73e1b32af
wonnuri <132761542+wonnuri@users.noreply.github.com>
2024-03-04 19:15:32 +0900

    Delete cnu_project directory

--- cnu_project/src/main/java/org/example/developer/repository/UniversityRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.developer.repository;
-
-import org.example.developer.domain.University;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UniversityRepository extends JpaRepository<University, Long> {
-}
